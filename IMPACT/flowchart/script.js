3e25f3c0608d2f595af07c71e9cbdeb0d4dbcfad
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-08-20 18:50:32 +0530

    Create script.js

---
+++ IMPACT/flowchart/script.js
@@ -0,0 +1,6 @@
+var m1 = document.getElementById("mark1").value;
+var m2 = document.getElementById("mark2").value;
+var m3 = document.getElementById("mark3").value;
+var m4 = document.getElementById("mark4").value;
+var sum = m1+m2+m3+m4;
+document.getElementsByTagName("button").onclick = window.alert(sum);
