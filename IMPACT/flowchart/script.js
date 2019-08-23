1d682deacf566f079044e4aad7b4447d54300b10
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-08-23 17:52:13 +0530

    Update script.js

--- IMPACT/flowchart/script.js
+++ IMPACT/flowchart/script.js
@@ -1,30 +1,87 @@
-var m1,m2,m3,m4;
-
-
-
-
+var m1=new Array();
+var m2=new Array();
+var m3=new Array();
+var m4=new Array();
+var sum=new Array();
+var average=new Array();
+//var fail=new Array();
+//var pass=new Array();
+//var totstu=new Array();
+var passpp=new Array();
+var gradcount=1;
+var grade=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+var i,y;
+var x=0;
+var pass=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+var fail=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+var passpp=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
+var totstu=["0","0","0","0","0","0","0","0","0","0","0","0","0"];
 
 function func()
 {
-    m1=parseInt(document.getElementById("mark1").value,10);
-    m2=parseInt(document.getElementById("mark2").value,10);
-    m3=parseInt(document.getElementById("mark3").value,10);
-    m4=parseInt(document.getElementById("mark4").value,10);
-    var sum=m1+m2+m3+m4;
-    document.getElementById("demo").value=sum;
-    var average=(sum/4);
-    document.getElementById("dem").value=average;
-    var pass=0,fail=0;
-    if (average>60)
+    grade[x]=document.getElementById("gr").value;
+    m1[x]=parseInt(document.getElementById("mark1").value);
+    m2[x]=parseInt(document.getElementById("mark2").value);
+    m3[x]=parseInt(document.getElementById("mark3").value);
+    m4[x]=parseInt(document.getElementById("mark4").value);
+    
+    check();
+    x++;
+}
+
+function check()
+{
+    sum[x]=m1[x] + m2[x] + m3[x] + m4[x];
+    document.getElementById("demo").value=sum[x];
+    average[x]=(sum[x]/4);
+    document.getElementById("dem").value=average[x];
+    if (average[x]>=60)
     {
-        pass=pass+1;
+        pass[grade[x]]++;
+        totstu[grade[x]]++;
     }
     else
     {
-        fail=fail+1;
+        fail[grade[x]]++;
+        totstu[grade[x]]++;
     }
-    document.getElementById("pass").value=pass;
-    document.getElementById("fail").value=fail;
+    passpp[grade[x]]=(pass[grade[x]]/totstu[grade[x]])*100;
     
+}
+
+function display()
+{
+    var print="<hr/>";
+    var k="Grade  No.of.students  pass fail passpercent <br/>";
+    for(i=1;i<=12;i++)
+    {
+    
+    
+    print+=i+totstu[i]+pass[i]+fail[i]+passpp[i]+"<br/>";
+    document.getElementById("Result").innerHTML=k+print;
+
+    document.getElementById("pass").value=pass[i];
+    document.getElementById("fail").value=fail[i];
+    document.getElementById("totstu").value=totstu[i];
+    document.getElementById("passpp").value=passpp[i];
+    }
+}
+
+
+function nextstu() 
+{
+   
+    document.getElementById("mark1").value="";
+    document.getElementById("mark2").value="";
+    document.getElementById("mark3").value="";
+    document.getElementById("mark4").value="";
+    document.getElementById("demo").value="";
+    document.getElementById("dem").value="";
+}
+
+ function assign()
+{  
+    gradcount = document.getElementById("gr").value;
+    document.getElementById("gd").value= gradcount;
 }
 
