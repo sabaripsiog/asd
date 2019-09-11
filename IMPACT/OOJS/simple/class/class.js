ee488b2cedabadbf42661cdeba762d9f41cb1b57
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-09-11 19:07:00 +0530

    Create class.js

---
+++ IMPACT/OOJS/simple/class/class.js
@@ -0,0 +1,25 @@
+function Television(brand,inch,price)
+{
+    this.brand = brand;
+    this.inch = inch;
+    this.price = price;
+    this.display = function (){
+        console.log(`This is ${this.brand} TV of size ${this.inch} inches and costs Rupees ${this.price}.`);
+    } 
+}
+
+// the above constructor function can be used create a class of objects
+
+
+const object1 = new Television('Onida', 40, 30000);
+
+object1.brand = 'LG'; //changes the brand name to 'LG'
+
+const object2 = new Television('Toshibha', 55, 60000);
+
+console.log(object1);
+object1.display();
+
+console.log(object2);
+object2.display();
+
