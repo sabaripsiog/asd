777c927bfe8040034b56c5fb70e494c3bbf3550f
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-09-17 20:11:02 +0530

    Create NewUser.cs

---
+++ IMPACT/C# fundamentals/Crud/Crud/NewUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud
+{
+    class NewUser
+    {
+        public string username;
+        public string pass;
+        public string getName()
+        {
+            Console.WriteLine("Enter New User name:");
+            username = Console.ReadLine();
+            return username;
+        }
+        public string getPass()
+        {
+            Console.WriteLine("Enter New Password:");
+            pass = Console.ReadLine();
+            return pass;
+        }
+    }
+}
