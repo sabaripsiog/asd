9cea566440b0018c1a45f875e9e385a307c87737
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-09-17 20:10:54 +0530

    Create Details.cs

---
+++ IMPACT/C# fundamentals/Crud/Crud/Details.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud
+{
+    class Details
+    {
+        public string customerID;
+        public string email;
+        public string address;
+        public void getDetails()
+        {
+            Console.WriteLine("Enter customer ID:");
+            customerID = Console.ReadLine();
+            Console.WriteLine("Enter email:");
+            email = Console.ReadLine();
+            Console.WriteLine("Enter address:");
+            address = Console.ReadLine();
+        }
+    }
+}
