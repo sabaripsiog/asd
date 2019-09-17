36f2a640666470e8f665bb5cd7344b79149f0601
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-09-17 20:11:11 +0530

    Create User.cs

---
+++ IMPACT/C# fundamentals/Crud/Crud/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud
+{
+    class User
+    {
+        public string name { get; set; }
+        public string pass { get; set; } 
+    }
+}
