784d21e178d3735652020ed0ab3efe9d6b0b62fb
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-09-19 19:51:13 +0530

    Update NewUser.cs

--- IMPACT/C# fundamentals/Crud/Crud/NewUser.cs
+++ IMPACT/C# fundamentals/Crud/Crud/NewUser.cs
@@ -10,17 +10,58 @@ namespace Crud
     {
         public string username;
         public string pass;
+        public string address;
+        public string customerType;
+        
+        
         public string getName()
         {
             Console.WriteLine("Enter New User name:");
             username = Console.ReadLine();
+            while (string.IsNullOrEmpty(username))
+            {
+                Console.WriteLine("Name can't be empty! Input your Username once more");
+                username = Console.ReadLine();
+            }
             return username;
         }
         public string getPass()
         {
             Console.WriteLine("Enter New Password:");
             pass = Console.ReadLine();
+            while (string.IsNullOrEmpty(pass))
+            {
+                Console.WriteLine("Password can't be empty! Input your password once more");
+                pass = Console.ReadLine();
+            }
             return pass;
+        }
+        public string getAddress()
+        {
+            Console.WriteLine("Enter your address:");
+            address = Console.ReadLine();
+            while (string.IsNullOrEmpty(address))
+            {
+                Console.WriteLine("Address can't be empty! Input your address once more");
+                address = Console.ReadLine();
+            }
+            return address;
+        }
+        public string getType()
+        {
+            Console.WriteLine("Enter Electricity type(Domestic or Commercial:");
+            customerType = Console.ReadLine();
+            while (string.IsNullOrEmpty(customerType))
+            {
+                Console.WriteLine("Password can't be empty! Input your password once more");
+                customerType = Console.ReadLine();
+            }
+            while (customerType != "Commercial" && customerType != "Domestic" && customerType != "commercial" && customerType != "domestic")
+            {
+                Console.WriteLine("Type should either Commercial or Domestic");
+                customerType = Console.ReadLine();
+            }
+            return customerType;
         }
     }
 }
