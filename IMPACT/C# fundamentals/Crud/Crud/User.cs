16ffc35e8b0ced321c960d84e8ed2c74fde3b298
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-09-19 19:51:22 +0530

    Update User.cs

--- IMPACT/C# fundamentals/Crud/Crud/User.cs
+++ IMPACT/C# fundamentals/Crud/Crud/User.cs
@@ -10,5 +10,11 @@ namespace Crud
     {
         public string name { get; set; }
         public string pass { get; set; } 
+        public int customerID { get; set; }
+        public string address { get; set; }
+        public string customerType { get; set; }
+        public int PreviousUnits { get; set; }
+        public int CurrentUnits { get; set; }
+
     }
 }
