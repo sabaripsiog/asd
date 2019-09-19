b81658e146fffdc331bf61e5cdb1e3024c8865f9
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-09-19 19:51:17 +0530

    Update Program.cs

--- IMPACT/C# fundamentals/Crud/Crud/Program.cs
+++ IMPACT/C# fundamentals/Crud/Crud/Program.cs
@@ -12,71 +12,18 @@ namespace Crud
         {
             List<User> Usernames = new List<User>();                                             //List for users
 
-            Usernames.Add(new User() { name = "Sab", pass = "pass1" });
-            Usernames.Add(new User() { name = "xyz", pass = "pass2" });
-            Usernames.Add(new User() { name = "abc", pass = "pass3" });
-            int userOption;
-            string Getoption;
-            string uname;
-            string passw;
-            NewUser new1 = new NewUser();
-            bool Confirm = false;
+            Usernames.Add(new User() { name = "sab", pass = "pass1", address = "8 , Raja street , Anna nagar.", customerID = 1 , customerType = "Domestic" , PreviousUnits = 600 , CurrentUnits = 680 });
+            Usernames.Add(new User() { name = "xyz", pass = "pass2", address = "1 , Nehru street , T nagar.", customerID = 2, customerType = "Commercial", PreviousUnits = 2134, CurrentUnits = 2433 });
+            Usernames.Add(new User() { name = "abc", pass = "pass3", address = "81 , MD street , Saidapet.", customerID = 3, customerType = "Domestic", PreviousUnits = 800, CurrentUnits = 931 });
+
+            Menu m1 = new Menu();
             Login log = new Login();
             Details info = new Details();
-            do
-            {
-                Console.WriteLine(" \t \t \t Welcome to TNEB Website!!");
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("1. Existing User ");
-                Console.WriteLine("2. New User ");
-                Console.WriteLine();
-                Console.WriteLine("Enter your option");
-
-                Getoption = Console.ReadLine();
-
-                while (!int.TryParse(Getoption, out userOption))
-                {
-                    Console.WriteLine("This is not a number!");
-                    Getoption = Console.ReadLine();
-                }
-
-                if ((userOption > 0) && (userOption < 3))
-                {
-                    Confirm = false;
-                    switch (userOption)
-                    {
-                        case 1:
-                            Console.Clear();
-                            log.Form(Usernames);
-                            break;
-
-                        case 2:
-                            Console.Clear();
-                            uname = new1.getName();
-                            passw = new1.getPass();
-                            Usernames.Add(new User() { name = uname, pass = passw });
-                            Console.Clear();
-                            Console.WriteLine("Registered Successfully");
-                            log.Form(Usernames);
-                            break;
-
-                        default:
-                            Console.WriteLine("No match found");
-                            break;
-                    }
-                }
-                else
-                {
-                    Confirm = true;
-                    Console.Clear();
-                    Console.WriteLine("Re-enter the options");
-                }
-
-            } while (Confirm == true);
-
+            UserType u1 = new UserType();
+            m1.display1(Usernames);
+            log.Form(Usernames);
             info.getDetails();
-
+            u1.select();
             Console.ReadKey();
         }
     }
