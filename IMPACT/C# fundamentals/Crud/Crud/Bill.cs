9d57aadefe6e43057111b53891a756b4c81bdc9d
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-09-23 19:48:15 +0530

    Update Bill.cs

--- IMPACT/C# fundamentals/Crud/Crud/Bill.cs
+++ IMPACT/C# fundamentals/Crud/Crud/Bill.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,22 +22,12 @@ namespace Crud
             Console.WriteLine("Press Enter to proceed to bill.");
             Console.ReadLine();
             Console.Clear();
-            
+
             
             PrintDetails(u);
-            doCalculation(u);
+           
         }
         public void PrintDetails(User u)
-        {
-            Console.WriteLine("---------------------------TNEB------------------------------");
-            Console.WriteLine("---------------------------BILL------------------------------");
-            Console.WriteLine("Date:"+DateTime.Now.ToString("dd/MM/yyyy")+ "\t\t\t\t\t"+"Time:"+ DateTime.Now.ToString("hh:mm tt"));
-            Console.WriteLine("-------------------------------------------------------------------");
-            Console.WriteLine($"Consumer name:{u.name}\t\t\t\tConsumer ID:{u.customerID}");
-            Console.WriteLine($"Consumer Address:{u.address}");
-        }
-
-        public void doCalculation(User u)
         {
             Units_this_month = u.CurrentUnits - u.PreviousUnits;
             if (u.customerType == "Domestic")
@@ -71,12 +62,47 @@ namespace Crud
             }
             Total = Rupees_per_unit * Units_this_month;
 
+
+
+
+            Console.WriteLine("---------------------------TNEB------------------------------");
+            Console.WriteLine("---------------------------BILL------------------------------");
+            Console.WriteLine("Date:" + DateTime.Now.ToString("dd/MM/yyyy") + "\t\t\t\t\t" + "Time:" + DateTime.Now.ToString("hh:mm tt"));
+            Console.WriteLine("-------------------------------------------------------------------");
+            Console.WriteLine($"Consumer name:{u.name}\t\t\t\tConsumer ID:{u.customerID}");
+            Console.WriteLine($"Consumer Address:{u.address}");
+
+
             Console.WriteLine("-------------------------------------------------------------------");
             Console.WriteLine($"Current consumption: \t\t\t\t {Units_this_month}");
             Console.WriteLine($"Per unit charge: \t\t\t\t {Rupees_per_unit}");
             Console.WriteLine("-------------------------------------------------------------------");
             Console.WriteLine($"Total charges: \t\t\t\t\t {Total}");
             Console.WriteLine("-------------------------------------------------------------------");
+
+
+            StreamWriter billfile = new StreamWriter(@"Bill.txt");
+
+            billfile.WriteLine("---------------------------TNEB------------------------------");
+            billfile.WriteLine("---------------------------BILL------------------------------");
+            billfile.WriteLine("Date:" + DateTime.Now.ToString("dd/MM/yyyy") + "\t\t\t\t\t" + "Time:" + DateTime.Now.ToString("hh:mm tt"));
+            billfile.WriteLine("-------------------------------------------------------------------");
+            billfile.WriteLine($"Consumer name:{u.name}\t\t\t\tConsumer ID:{u.customerID}");
+            billfile.WriteLine($"Consumer Address:{u.address}");
+            
+
+            billfile.WriteLine("-------------------------------------------------------------------");
+            billfile.WriteLine($"Current consumption: \t\t\t\t {Units_this_month}");
+            billfile.WriteLine($"Per unit charge: \t\t\t\t {Rupees_per_unit}");
+            billfile.WriteLine("-------------------------------------------------------------------");
+            billfile.WriteLine($"Total charges: \t\t\t\t\t {Total}");
+            billfile.WriteLine("-------------------------------------------------------------------");
+            billfile.Close();
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            Console.WriteLine("Press enter to exit");
+            
         }
     }
 }
