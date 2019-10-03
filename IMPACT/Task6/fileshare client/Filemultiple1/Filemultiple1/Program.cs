d24ef723b87ce8afba34ae73dc5f47114dfdd73f
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-10-03 19:20:32 +0530

    Update Program.cs

--- IMPACT/Task6/fileshare client/Filemultiple1/Filemultiple1/Program.cs
+++ IMPACT/Task6/fileshare client/Filemultiple1/Filemultiple1/Program.cs
@@ -13,48 +13,30 @@ namespace Client_Socket
         {
             try
             {
-            
-                    Console.WriteLine("That program can transfer small file. I've test up to 850kb file");
-                    IPAddress[] ipAddress = Dns.GetHostAddresses("192.168.153.57");
-                    IPEndPoint ipEnd = new IPEndPoint(ipAddress[0], 8100);
-                    Socket clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
-               
-                    clientSock.Connect(ipEnd);
+                //Console.WriteLine("That program can transfer small file. I've test up to 850kb file");
+                IPAddress[] ipAddress = Dns.GetHostAddresses("192.168.153.57");
+                IPEndPoint ipEnd = new IPEndPoint(ipAddress[0], 5656);
+                Socket clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                clientSock.Connect(ipEnd);
 
-
-                    Console.WriteLine("Enter the file name to be sent:");
-                    string fileName;
-                    string filePath = @"C:\Users\Sabarish.a\Desktop\extras\";//Your File Path;
-
-                    while (!string.IsNullOrEmpty((fileName = Console.ReadLine())))
-                    {
-                        byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
-
-                        byte[] fileData = File.ReadAllBytes(filePath + fileName);
-                        byte[] clientData = new byte[4 + fileNameByte.Length + fileData.Length];
-                        byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
-
-                        fileNameLen.CopyTo(clientData, 0);
-                        fileNameByte.CopyTo(clientData, 4);
-                        fileData.CopyTo(clientData, 4 + fileNameByte.Length);
-
-
-                        clientSock.Send(clientData);
-                        Console.WriteLine("File:{0} has been sent.", fileName);
-                    }
-
-                    clientSock.Close();
-                    Console.ReadLine();
-                    
-              
+                byte[] clientData = new byte[1024 * 50000];
+                string receivedPath = "C:/Users/Sabarish.a/Desktop/New/";
+                int receivedBytesLen = clientSock.Receive(clientData);
+                int fileNameLen = BitConverter.ToInt32(clientData, 0);
+                string fileName = Encoding.ASCII.GetString(clientData, 4, fileNameLen);
+                Console.WriteLine("Client:{0} connected & File {1} started received.", clientSock.RemoteEndPoint, fileName);
+                BinaryWriter bWrite = new BinaryWriter(File.Open(receivedPath + fileName, FileMode.Append)); ;
+                bWrite.Write(clientData, 4 + fileNameLen, receivedBytesLen - 4 - fileNameLen);
+                Console.WriteLine("File: {0} received & saved at path: {1}", fileName, receivedPath);
+                bWrite.Close();
+                clientSock.Close();
+                Console.ReadLine();
+                Console.ReadKey();
             }
             catch (Exception ex)
             {
-                Console.WriteLine("File Receiving fail." + ex.Message);
-                
-                
+                Console.WriteLine("File Sending fail." + ex.Message);
             }
-
         }
     }
 }
