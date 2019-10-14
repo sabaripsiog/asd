d75aaa2be9e38cfee44f155e8363f0112fb09f2f
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-10-14 16:12:35 +0530

    Update Program.cs

--- IMPACT/Task6/File3/File3/Program.cs
+++ IMPACT/Task6/File3/File3/Program.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -24,12 +24,15 @@ namespace Lastsampleclient
             
             NetworkStream ns = client.GetStream();
             Thread thread = new Thread(o => ReceiveData((TcpClient)o));
-            //Thread thread1 = new Thread(o => fileReceiveData((TcpClient)o));
             thread.Start(client);
+            //Thread thread1 = new Thread(o => Conversation.clientnames((TcpClient)o));
+
             Console.WriteLine("client connected!!");
             //thread1.Start(client);
+            
             string s;
             string name;
+            
             //string sender;
             Console.WriteLine("Enter the user name:");
             name = Console.ReadLine();
@@ -45,7 +48,7 @@ namespace Lastsampleclient
             client.Client.Send(nameBuffer);
             Array.Clear(nameBuffer, 0, nameBuffer.Length);
 
-
+            
 
             while (!string.IsNullOrEmpty((s = Console.ReadLine())))
             {
@@ -71,6 +74,11 @@ namespace Lastsampleclient
             Console.WriteLine("disconnect from server!!");
             Console.ReadKey();
         }
+
+
+       
+
+
 
         public class Conversation
         {
@@ -150,16 +158,34 @@ namespace Lastsampleclient
 
 
             }
+            public static void clientnames(TcpClient client)
+            {
+               
+                while (true)
+                {
+                   
+                    NetworkStream ns = client.GetStream();
+                    byte[] receivedBytes = new byte[1024];
+                    //byte[] receivedbytes1 = new byte[1024 * 5000];
+                    int byte_count;
+                    if ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+                    {
+       
+                        Console.WriteLine(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+                    }
+                    Array.Clear(receivedBytes,0,receivedBytes.Length);
+                }
+            }
         }
 
         static void ReceiveData(TcpClient client)
         {
-            while (true)
+            
 
+            while (true)
             {
                 try
                 {
-
                     byte[] buffer = new byte[1024];
                     int byte_count1 = client.Client.Receive(buffer);
                     string messagedata = Encoding.ASCII.GetString(buffer, 0, byte_count1);
