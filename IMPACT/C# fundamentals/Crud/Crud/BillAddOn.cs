6b225c3d76690e646488cf192b9119513b432b36
sabaripsiog <53895965+sabaripsiog@users.noreply.github.com>
2019-10-30 15:28:43 +0530

    Create BillAddOn.cs

---
+++ IMPACT/C# fundamentals/Crud/Crud/BillAddOn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud
+{
+    public class BillAddOn
+    {
+        public int UnitPerMonth { get; set; }
+        public double RupeesPerUnit { get; set; }
+        public double Total { get; set; }
+    }
+}
